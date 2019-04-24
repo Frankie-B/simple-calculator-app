e3313d0c9d0ed9b539103f49de8f9fbcbdc45c2a
Frankie Bukenya <frankie.bukenya@gmail.com>
2019-04-24 21:17:01 +0300

    Add Req and Res for server

--- calculator.js
+++ calculator.js
@@ -7,3 +7,8 @@ const app = express();
 app.listen(3000, function () {
     console.log('Server started on port 3000');
 });
+
+// Root route
+app.get('/',  function(req, res) {
+    res.send('Hello World!');
+});
