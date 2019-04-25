22e5815c28911f4c0b652d2ecd1fc0e5e8547524
Frankie Bukenya <frankie.bukenya@gmail.com>
2019-04-25 06:19:22 +0300

    Add Calculator start files

--- calculator.js
+++ calculator.js
@@ -4,11 +4,11 @@ const express = require('express');
 
 const app = express();
 
-app.listen(3000, function () {
-    console.log('Server started on port 3000');
+app.listen(3000, function() {
+  console.log('Server started on port 3000');
 });
 
 // Root route
-app.get('/',  function(req, res) {
-    res.send('Hello World!');
+app.get('/', function(req, res) {
+  res.sendFile(__dirname + '/index.html');
 });
