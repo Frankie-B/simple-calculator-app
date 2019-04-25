c4d1cd129036de557ad8fc6361dfcc403ff94f3d
Frankie Bukenya <frankie.bukenya@gmail.com>
2019-04-25 06:21:33 +0300

    Add Response route for inde.html

--- calculator.js
+++ calculator.js
@@ -8,7 +8,7 @@ app.listen(3000, function() {
   console.log('Server started on port 3000');
 });
 
-// Root route
+// Root route to index.html using SendFile
 app.get('/', function(req, res) {
   res.sendFile(__dirname + '/index.html');
 });
